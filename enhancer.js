f9523bfcb86423dbe5a7b9305c5bdb596d155846
rjs76 <35549025+rjs76@users.noreply.github.com>
2019-04-21 23:48:45 -0400

    create enhancer.js

---
+++ enhancer.js
@@ -0,0 +1,16 @@
+//making 4 lists and having a way to sort them using an array
+let log = console.log;
+//list of arrays
+var list2 = ["Class 1", "Meeting 1","Club Meeting 1"];
+var list3 = ["Class 2", "meeting 2","Club Meeting 2"];
+var list4 = ["Class 3", "Meeting 3","Club Meeting 3"];
+var list5 = ["Class 4", "Meeting 4","Club Meeting 4"];
+
+//sorting each one
+log (list2.sort());
+log (list3.sort());
+log (list4.sort());
+log (list5.sort());
+
+//checks for filter by word
+const result = words.filter(word => word.length > 6);
